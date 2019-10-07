47a01860ec065551e6b3209db2b04103594a61d5
raythurman2386 <raymondthurman5@gmail.com>
2019-10-07 11:45:25 -0400

    local storage hook initially complete

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,9 +2,18 @@ import { useState } from 'react'
 
 const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
+    // check for the item in localstorage
     const item = window.localStorage.getItem(key)
+    // return stored item or the initial value
     return item ? JSON.parse(item) : initialValue
   })
 
-  return [storedValue]
+  const setValue = value => {
+    // set the value to state
+    setStoredValue(value)
+    // save the value to local storage
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  return [storedValue, setValue]
 }
