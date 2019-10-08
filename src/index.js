396c5d23cb8dcaff1e07ba0e428c375053c02b6e
raythurman2386 <raymondthurman5@gmail.com>
2019-10-08 19:37:37 -0400

    split off app into it's own component

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
+import './styles.scss'
 
-import "./styles.scss";
-
-const App = () => {
-  const [coinData, setCoinData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
-  return (
-    <div className="App">
-      <Navbar />
-      <Charts coinData={coinData} />
-    </div>
-  );
-};
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
