bc8f3e831d21ef0fc58b8d0d11c823873aa05c3a
raythurman2386 <raymondthurman5@gmail.com>
2019-10-07 21:46:09 -0400

    starting dark mode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+import { useLocalStorage } from './useLocalStorage'
+
+const useDarkMode = () => {
+  const [value, setValue] = useLocalStorage('')
+
+  // return the destructured array
+  return []
+}
