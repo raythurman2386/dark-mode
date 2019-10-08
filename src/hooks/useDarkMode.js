35d103af58cc53907a022d2880aa75d2d198431d
raythurman2386 <raymondthurman5@gmail.com>
2019-10-07 22:03:07 -0400

    possible dark mode complete ???

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,9 +1,16 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
-const useDarkMode = () => {
-  const [value, setValue] = useLocalStorage('')
+export const useDarkMode = () => {
+  // save darkmode to local storage
+  const [darkValue, setDarkValue] = useLocalStorage('dark')
+
+  useEffect(() => {
+    darkValue
+      ? document.querySelector(body).classList.add('dark-mode')
+      : document.querySelector(body).classList.remove('dark-mode')
+  }, [darkValue])
 
   // return the destructured array
-  return []
+  return [darkValue, setDarkValue]
 }
