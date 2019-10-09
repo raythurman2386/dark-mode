d2aa49c004bbd6a4afdd17d882db3cf8e504836c
raythurman2386 <raymondthurman5@gmail.com>
2019-10-08 20:24:04 -0400

    touched up a few styles to match dark and light

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='charts'>
+    <div className='home'>
       <h1>Welcome to Crypto Tracker!!</h1>
       <h2>Your best source for tracking all your crypto prices!</h2>
       <Link to='/allcharts'>Click Here to check out the Charts!</Link>
