dce8f2819041daa1594c0352ef64b3f7a123bd6d
raythurman2386 <raymondthurman5@gmail.com>
2019-10-08 20:36:53 -0400

    made axios custom hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './components/Home'
 import Charts from './components/Charts'
 import Navbar from './components/Navbar'
+import { useAxios } from './hooks/useAxios'
 
 const App = () => {
-  const [coinData, setCoinData] = useState([])
+  const [coinData] = useAxios()
 
-  useEffect(() => {
-    axios
-      .get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err))
-  }, [])
   return (
     <div className='App'>
       <Navbar />
